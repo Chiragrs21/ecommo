d822d2fed878019169a3b297843d9f4d65b60527
chirag r_s <chiragrs2003@gmail.com>
2023-11-10 15:45:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='ecommo/' element={<Home />} />
       </Routes>
     </>
   );
